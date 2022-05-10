46b168b337b5fdd4a8a30ef81a9104b1ae54e242
Hatatori Nitsukawa <kcireh.i@gmail.com>
2022-05-10 03:24:20 -0300

    Update csv2json.js

--- csv2json.js
+++ csv2json.js
@@ -1,12 +1,21 @@
-function csv2json(txt){
+/*
+csv = `location,date,variant,num_sequences,perc_sequences,num_sequences_total
+Angola,2020-07-06,Alpha,0,0.0,3
+Angola,2020-07-06,B.1.1.277,0,0.0,3
+Angola,2020-07-06,B.1.1.302,0,0.0,3
+Angola,2020-07-06,B.1.1.519,0,0.0,3
+Angola,2020-07-06,B.1.160,0,0.0,3
+Angola,2020-07-06,B.1.177,0,0.0,3`
+*/
+
+function csv2json(csv, sep = ','){
     let ar = []
-    let obj = {}
-    let sep = ","
-
-    let cols = txt.split("\n")[0].split(sep)
-    let lines = txt.split("\n").slice(1).map(e=>e.split(sep))
-
+    
+    let cols = csv.split("\n")[0].split(sep)
+    let lines = csv.split("\n").slice(1).map(e=>e.split(sep))
+    
     function Merge(a,b){
+        let obj = {}
         a.map((e,i)=>obj[e] = b[i]) 
         return obj
     }
@@ -16,4 +25,8 @@ function csv2json(txt){
     return ar
 }
 
-export default csv2json;
+
+// console.log(csv2json(csv))
+
+
+export default csv2json

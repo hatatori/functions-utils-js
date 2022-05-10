1ce3df10f5cdbd99c5c1eaf471c8e7ca871aa58d
Hatatori Nitsukawa <kcireh.i@gmail.com>
2022-05-10 03:09:10 -0300

    Add files via upload

--- csv2json.js
+++ csv2json.js
@@ -1,19 +1,10 @@
-// csv = `location,date,variant,num_sequences,perc_sequences,num_sequences_total
-// Angola,2020-07-06,Alpha,0,0.0,3
-// Angola,2020-07-06,B.1.1.277,0,0.0,3
-// Angola,2020-07-06,B.1.1.302,0,0.0,3
-// Angola,2020-07-06,B.1.1.519,0,0.0,3
-// Angola,2020-07-06,B.1.160,0,0.0,3
-// Angola,2020-07-06,B.1.177,0,0.0,3
-// Angola,2020-07-06,B.1.221,0,0.0,3`
-
 function csv2json(txt){
     let ar = []
     let obj = {}
     let sep = ","
 
-    let cols = csv.split("\n")[0].split(sep)
-    let lines = csv.split("\n").slice(1).map(e=>e.split(sep))
+    let cols = txt.split("\n")[0].split(sep)
+    let lines = txt.split("\n").slice(1).map(e=>e.split(sep))
 
     function Merge(a,b){
         a.map((e,i)=>obj[e] = b[i]) 
@@ -25,4 +16,4 @@ function csv2json(txt){
     return ar
 }
 
-// console.log(csv2json(csv))
+export default csv2json;

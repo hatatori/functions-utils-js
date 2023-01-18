7feed519f73f82263e999dea2ebdc6ff43ebeea7
Hatatori Nitsukawa <kcireh.i@gmail.com>
2023-01-18 17:09:28 -0300

    Add files via upload

--- test.mjs
+++ test.mjs
@@ -1,15 +1,11 @@
 import csvtoarray from './csvtoarray.mjs'
 import querystrintoobj from './querystrintoobj.mjs'
-// let csvtoarray = require('./csvtoarray.js')
-// let csvtoarray = require('./csvtoarray')
 
 // csvtoarray
 let csv = `location,date,variant,num_sequences,perc_sequences,num_sequences_total
 Angola,2020-07-06,Alpha,0,0.0,3
 Angola,2020-07-06,B.1.1.277,0,0.0,3`
-
 console.log('test 1 - csvtoarray')
-
 console.log(csvtoarray(csv))
 
 // querystrintoobj

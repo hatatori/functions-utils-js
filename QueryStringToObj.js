b373264599d72337a04b28978683e6496d5c7b1b
Hatatori Nitsukawa <kcireh.i@gmail.com>
2023-01-17 22:22:48 -0300

    Update QueryStringToObj.js

--- QueryStringToObj.js
+++ QueryStringToObj.js
@@ -1,14 +1,12 @@
-// Query String To Object
-function QueryStringToObj(str) {
-  let query = str.split("?").at(-1);
-  let obj = query.split("&");
-  let b = {};
-  obj = obj.map((e) => {
-    a = e.split("=");
-    a[1] = !isNaN(Number(a[1])) ? Number(a[1]) : a[1];
-    b[a[0]] = a[1];
-  });
-  return b;
+function StrToObj(str){
+  let arr = str.split("&")
+  let c = {}
+  arr.map(e=>{
+      let a = e.split("=")[0]
+      let b = e.split("=")[1]
+      c[a] = b
+  })
+  return c
 }
 
-QueryStringToObj("https://hatatori.github.io/market/market-cdn.json?category=alimentos&qt=10");
+export default StrToObj

55a9ee86df091db701176609bf901cdc000edc77
Hatatori Nitsukawa <kcireh.i@gmail.com>
2023-01-19 21:10:07 -0300

    Update csvtoarray.mjs

--- csvtoarray.mjs
+++ csvtoarray.mjs
@@ -2,7 +2,7 @@
 // Angola,2020-07-06,Alpha,0,0.0,3
 // Angola,2020-07-06,B.1.1.277,0,0.0,3`
 
-function csvtoarray(csv, sep1=",", sep2="\n"){
+function csvtoarray(csv, sep=","){
     function arToObj(array1, array2){
         let c = {}
         array1.map((e,i)=> c[array1[i]] = array2[i] )
@@ -10,17 +10,17 @@ function csvtoarray(csv, sep1=",", sep2="\n"){
     }
 
     let csv2 = csv
-    let colunas = csv2.split("\n").shift().split(",")
+    let colunas = csv2.split("\n").shift().split(sep)
     let linhas = csv2.split("\n")
     let lista = []
+
     for(let i of linhas) {
-        let linha_array = i.split(sep1)
+        let linha_array = i.split(sep)
         let ob = arToObj(colunas, linha_array)
+        delete ob['\r']
         lista.push(ob)
     }
     return lista
 }
 
 export default csvtoarray
-// module.exports = csvtoarray; 
-// exports.imprimir = csvtoarray
